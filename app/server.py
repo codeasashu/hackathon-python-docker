d77013cf4024a287959621fa3e7b77c6177ecda1
Ashutosh <216.ashutosh@gmail.com>
2018-11-15 13:14:18 +0530

    Static files nginx serving app server config moved to configs

--- app/server.py
+++ app/server.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./frontend')
 
 
 @app.route('/')
 def hello():
-    return render_template("frontend/index.html")
+    return render_template('dist/index.html')
 
 if __name__ == '__main__':
     print("Server running at"+ os.environ['HOST'] +":"+ os.environ['PORT'])
-    app.run(host=os.environ['HOST'], port=os.environ['PORT'])
+    app.run(host=os.environ['HOST'], port=os.environ['PORT'], debug=True)
