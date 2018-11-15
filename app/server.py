4eac900ff80909b505fc03ba5e042f0af87fc9d3
Ashutosh <216.ashutosh@gmail.com>
2018-11-15 12:24:43 +0530

    Added NodeJS, npm, Vue

--- app/server.py
+++ app/server.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("frontend/index.html")
 
 if __name__ == '__main__':
     print("Server running at"+ os.environ['HOST'] +":"+ os.environ['PORT'])
